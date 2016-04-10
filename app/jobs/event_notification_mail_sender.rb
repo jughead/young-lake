ca9596c924c35462cc7249aa80c88ca0ccc201b7
Alexander Fedulin <alexander.fedulin@gmail.com>
2016-04-10 22:28:19 +0300

    Отправляем почту только старым непрочитанным уведомлениям

--- app/jobs/event_notification_mail_sender.rb
+++ app/jobs/event_notification_mail_sender.rb
@@ -2,7 +2,7 @@ class EventNotificationMailSender < ActiveJob::Base
   # TODO: могут несколько запуститься,
   # если одна из них долго работает.
   def perform
-    EventNotification.not_sent.find_each do |event_notification|
+    EventNotification.not_sent.created_before(15.minutes.ago).find_each do |event_notification|
       EventNotificationMailDelivery.perform_later(event_notification)
     end
   end
--- app/models/event_notification.rb
+++ app/models/event_notification.rb
@@ -4,6 +4,9 @@ class EventNotification < ActiveRecord::Base
   validates :user, presence: true, uniqueness: {scope: :event_id}
   validates :event, presence: true
   scope :not_sent, ->{ where(sent_at: nil) }
+  scope :created_before, ->time{
+    where(arel_table[:created_at].lt(time))
+  }
 
   def mark_as_read
     self.is_read = true
